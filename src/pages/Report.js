1122e41bd953c636ea2e9d9a6e0cebdedae30d95
mycinus <45410115+mycinus@users.noreply.github.com>
2020-04-21 23:22:49 -0700

    created a chart example for the report

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import ReportTable from '../components/ReportTable';
+import ReportChart from '../components/ReportChart';
+
 
 const Report = () => {
    var curr = new Date();
     return (
-       <div>
+
+
+       <div id='content'>
           <h1>Your Noise Report Summary</h1>
+
+
           <p>Report for: { curr.getMonth() }/{ curr.getDate() }/{ curr.getFullYear() }</p>
+
+
+         <ReportChart/>
+
+
+          <ReportTable/>
        </div>
     );
 }
