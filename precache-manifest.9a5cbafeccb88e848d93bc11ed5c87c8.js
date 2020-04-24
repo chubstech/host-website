7c6a606ab7bc9cf9cf00d906e4437d51d3ebe1a2
Anna Takei <atakei@annas-mbp.lan>
2020-04-23 23:13:23 -0700

    Updates

---
+++ precache-manifest.9a5cbafeccb88e848d93bc11ed5c87c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cd10609d9a1ff8e53cf917df5f86fb",
+    "url": "/host-website/index.html"
+  },
+  {
+    "revision": "ed0341a8585960f1d60c",
+    "url": "/host-website/static/css/main.d85f3710.chunk.css"
+  },
+  {
+    "revision": "d980ce441696ab3dfb74",
+    "url": "/host-website/static/js/2.de13c19e.chunk.js"
+  },
+  {
+    "revision": "ca828177c607e4e2f857d061ad13f184",
+    "url": "/host-website/static/js/2.de13c19e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0341a8585960f1d60c",
+    "url": "/host-website/static/js/main.166684e2.chunk.js"
+  },
+  {
+    "revision": "ed6d7d981c029cbd95dd",
+    "url": "/host-website/static/js/runtime-main.3fe7bfe5.js"
+  }
+]);
