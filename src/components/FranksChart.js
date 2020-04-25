918a3773fe262c729b0ef12b6a2aca194a0186da
Frank Pena <fpenaaya@uci.edu>
2020-04-25 13:21:25 -0700

    Added Historical Graph

---
+++ src/components/FranksChart.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Chart from "chart.js";
+var Component = React.Component;
+
+
+export default class FrankLineGraph extends Component {
+    chartRef = React.createRef();
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+				var jsonfile = require('../test_data.json');
+				var dict = new Object();
+				var labels = jsonfile.map(function(e) {
+					dict[e.time_obs] = e.db_reading;
+					if(dict[e.time_obs] < e.db_reading)
+					{
+						dict[e.time_obs] = e.db_reading;
+					}
+					var dateObj = new Date(e.time_obs * 1000);
+					var hours = dateObj.getUTCHours();
+					// Get minutes part from the timestamp
+					var minutes = dateObj.getUTCMinutes();
+
+					// Get seconds part from the timestamp
+					var seconds = dateObj.getUTCSeconds();
+					var formattedTime = hours.toString().padStart(2, '0') + ':' +
+                minutes.toString().padStart(2, '0') + ':' +
+                seconds.toString().padStart(2, '0');
+				  return formattedTime
+				});
+				var data = jsonfile.map(function(e) {
+				   return dict[e.time_obs]
+				});;
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: labels,
+                datasets: [
+                    {
+                        label: "DB Levels",
+                        data: data,
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import Realtime from '../components/Realtime';
 import Baselines from '../components/Baselines';
+import FrankLineGraph from '../components/FranksChart'
 
 const Home = () => {
     return (
@@ -9,11 +10,14 @@ const Home = () => {
         <h1 class="no-print">Noise Manager WebApp</h1>
           <p>Real time data of each section/device</p>
           <div className="App-header">
+              <FrankLineGraph />
+              <br/>
               <Realtime />
               <Baselines />
             </div>
        </div>
     );
+
 }
 
 export default Home;
