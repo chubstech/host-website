d9c2f33d06f556374f4a0c33b0ad333614d360fd
mycinus <45410115+mycinus@users.noreply.github.com>
2020-04-26 16:12:35 -0700

    Updated ReportChart.js on the Report page

--- src/components/ReportChart.js
+++ src/components/ReportChart.js
@@ -1,150 +1,209 @@
 import React from 'react';
 import { TimeSeries } from "pondjs";
+import _ from "underscore";
 import {
     Charts,
     ChartContainer,
     ChartRow,
     YAxis,
-    LineChart,
     Baseline,
     Resizable
   } from "react-timeseries-charts";
-
-
-  const data = require("./usd_vs_euro.json");
-  const points = data.widget[0].data.reverse();
-  const series = new TimeSeries({
-      name: "USD_vs_EURO",
-      columns: ["time", "value"],
-      points
-  });
-
-  const style = {
-      value: {
-          stroke: "#a02c2c",
-          opacity: 0.2
-      }
-  };
-
-  const baselineStyle = {
-      line: {
-          stroke: "steelblue",
-          strokeWidth: 1,
-          opacity: 0.4,
-          strokeDasharray: "none"
-      },
-      label: {
-          fill: "steelblue"
-      }
-  };
-
-  const baselineStyleLite = {
-      line: {
-          stroke: "steelblue",
-          strokeWidth: 1,
-          opacity: 0.5
-      },
-      label: {
-          fill: "steelblue"
-      }
-  };
-
-  const baselineStyleExtraLite = {
-      line: {
-          stroke: "steelblue",
-          strokeWidth: 1,
-          opacity: 0.2,
-          strokeDasharray: "1,1"
-      },
-      label: {
-          fill: "steelblue"
-      }
-  };
-
-  class ReportChart extends React.Component {
-      state = {
-          tracker: null,
-          timerange: series.range()
-      };
-
-      handleTrackerChanged = tracker => {
-          this.setState({ tracker });
-      };
-
-      handleTimeRangeChange = timerange => {
-          this.setState({ timerange });
-      };
+  
+ import styler from './../js/styler' 
+import Legend from './../Legend'
+import LineChart from './LineChart'
 
 
 
-
-
-      render() {
-          return (
+const test_data = require('./../test_data.json');
 
 
 
-              <Resizable>
-                  <ChartContainer
-                      title="Daily Noise Report"
-                      titleStyle={{ fill: "#555", fontWeight: 500 }}
-                      timeRange={series.range()}
-                      format="%b '%y"
-                      timeAxisTickCount={5}
-                  >
-                      <ChartRow height="200">
-                          <YAxis
-                              id="price"
-                              label="dB Level (Noise)"
-                              min={series.min()}
-                              max={series.max()}
-                              width="50"
-                              format=",.2f"
-                          />
-                          <Charts>
-                              <LineChart axis="price" series={series} style={style} />
-                              <Baseline
-                                  axis="price"
-                                  style={baselineStyleLite}
-                                  value={series.max()}
-                                  label="Max"
-                                  position="right"
-                              />
-                              <Baseline
-                                  axis="price"
-                                  style={baselineStyleLite}
-                                  value={series.min()}
-                                  label="Min"
-                                  position="right"
-                              />
-                              <Baseline
-                                  axis="price"
-                                  style={baselineStyleExtraLite}
-                                  value={series.avg() - series.stdev()}
-                              />
-                              <Baseline
-                                  axis="price"
-                                  style={baselineStyleExtraLite}
-                                  value={series.avg() + series.stdev()}
-                              />
-                              <Baseline
-                                  axis="price"
-                                  style={baselineStyle}
-                                  value={series.avg()}
-                                  label="Avg"
-                                  position="right"
-                              />
-                          </Charts>
-                      </ChartRow>
-                  </ChartContainer>
-              </Resizable>
-          );
-      }
-  }
+// function getObsTime() {
+//    var myDate = new Date( timeobs *100);
+
+
+// }
+
+
+
+function buildPoints() {
+
+    let result = [];
+
+    for(let i = 0; i < test_data.length ; i++) {
+
+        var time = new Date( test_data[i].time_obs *100 );
+        result.push( [time,  test_data[i].db_reading, (test_data[i].db_reading -10) ] );
+
+    }
+
+    return result;
+
+
+}
+
+const noiseSeries = new TimeSeries({
+    name: "Noise",
+    columns: ["time", "debug1"],  //columns: ["time", "debug1", "debug2"],
+    points: buildPoints()
+});
+
+const style = styler([
+    { key: "debug1", color: "steelblue", width: 3 },
+    // { key: "debug2", color: "#F68B24", width: 2 }
+]);
+
+
+class ReportChart extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tracker: null,
+            timerange: noiseSeries.range(),
+            x: null,
+            y: null
+        };
+
+    }
+    handleTrackerChanged = tracker => {
+        if (!tracker) {
+            this.setState({ tracker, x: null, y: null });
+        } else {
+            this.setState({ tracker });
+        }
+    };
+
+    handleTimeRangeChange = timerange => {
+        this.setState({ timerange });
+    };
+
+    handleMouseMove = (x, y) => {
+        this.setState({ x, y });
+    };
+
+    render() {
+        const range = this.state.timerange;
+
+
+        return (
+            <div>
+
+                <p><strong>*Scroll up to Zoom in chart</strong></p>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <Resizable>
+                            <ChartContainer
+                                timeRange={range}
+                                timeAxisStyle={{
+                                    ticks: {
+                                        stroke: "black",
+                                        opacity: 0.25,
+                                        "stroke-dasharray": "1,1"
+                                        // Note: this isn't in camel case because this is
+                                        // passed into d3's style
+                                    },
+                                    values: {
+                                        fill: "black",
+                                        "font-size": 12
+                                    }
+                                }}
+                                showGrid={true}
+                                paddingRight={125}
+                                maxTime={noiseSeries.range().end()}
+                                minTime={noiseSeries.range().begin()}
+                                timeAxisAngledLabels={true}
+                                timeAxisHeight={65}
+                                onTrackerChanged={this.handleTrackerChanged}
+                                onBackgroundClick={() => this.setState({ selection: null })}
+                                enablePanZoom={true}
+                                onTimeRangeChanged={this.handleTimeRangeChange}
+                                onMouseMove={(x, y) => this.handleMouseMove(x, y)}
+                                minDuration={1000 * 60 * 60 * 24 * 30}
+                            >
+                                <ChartRow height="400">
+                                    <YAxis
+                                        id="y"
+                                        label="(dB) Noise Level"
+                                        min={0}
+                                        max={100}
+                                        style={{
+                                            ticks: {
+                                                stroke: "#AAA",
+                                                opacity: 0.25,
+                                                "stroke-dasharray": "1,1"
+                                                // Note: this isn't in camel case because this is
+                                                // passed into d3's style
+                                            }
+       
+                                        }}
+                                        showGrid
+                                        hideAxisLine
+                                        width="50"
+                                        type="linear"
+                                        format=""
+                                    />
+                                    <Charts>
+                                        <LineChart
+                                            axis="y"
+                                            breakLine={false}
+                                            series={noiseSeries}
+                                            columns={["debug1"]} //columns={["debug1", "debug2"]}
+                                            style={style}
+                                            interpolation="curveBasis"
+                                            highlight={this.state.highlight}
+                                            onHighlightChange={highlight =>
+                                                this.setState({ highlight })
+                                            }
+                                            selection={this.state.selection}
+                                            onSelectionChange={selection =>
+                                                this.setState({ selection })
+                                            }
+                                        />
+                                        <Baseline
+                                            axis="y"
+                                            value={70}
+                                            label="Noise (dB) Level Cap"
+                                            position="right"
+
+                                        />
+                                    </Charts>
+                                </ChartRow>
+                            </ChartContainer>
+                        </Resizable>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <span>
+                            <Legend
+                                type="line"
+                                align="right"
+                                style={style}
+                                highlight={this.state.highlight}
+                                onHighlightChange={highlight => this.setState({ highlight })}
+                                selection={this.state.selection}
+                                onSelectionChange={selection => this.setState({ selection })}
+                                categories={[
+                                    { key: "debug1", label: "User1 (Debug)"},
+                                    // { key: "debug2", label: "User2"}
+                                ]}
+                            />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 
 
 
 
-
+  
   export default ReportChart;
--- src/pages/Report.js
+++ src/pages/Report.js
@@ -3,25 +3,35 @@ import ReportTable from '../components/ReportTable';
 import ReportChart from '../components/ReportChart';
 import Button from 'react-bootstrap/Button';
 
-
 const Report = () => {
+   var curr = new Date();
+
+
    var curr = new Date();
    function printPage(e) {
      e.preventDefault();
      window.print();
    }
+
+
     return (
-       <div>
-        <div class="no-print">
-          <Button variant="info" className="print-btn" onClick={printPage}>Print report</Button>
-        </div>
 
-          <h1>Your Noise Report Summary</h1>
-          {/*JS Date() declared January the 0th month*/}
-          <p>Report for: { curr.getMonth()+1 }/{ curr.getDate() }/{ curr.getFullYear() }</p>
 
-          <ReportChart/>
+       <div id='content'>
+
+          <div class="no-print">
+            <Button variant="info" className="print-btn" onClick={printPage}>Print report</Button>
+          </div>
+
+          <h1>Your Noise Report Summary: { curr.getMonth() }/{ curr.getDate() }/{ curr.getFullYear() }</h1>
+
+         <div style={{border: '1px solid black', padding: 50, margin: 30}}>
+            <ReportChart/>
+         </div>
+
+         <div style={{padding: 20, margin: 30}}>
           <ReportTable/>
+         </div>
        </div>
     );
 }
