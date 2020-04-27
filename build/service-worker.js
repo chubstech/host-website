d0279f91bdaa7c7d5fe129be5d61c82a827d19ec
mycinus <45410115+mycinus@users.noreply.github.com>
2020-04-26 17:52:56 -0700

    Fixing website structure

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/host-website/precache-manifest.1ed007fdb7928e70175a4d665c6dc6cb.js"
+  "/host-website/precache-manifest.e04b9c6d5fbeaecf6ac483b5f1504b31.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/host-website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
