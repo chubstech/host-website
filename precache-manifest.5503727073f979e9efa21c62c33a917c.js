d8e297b0d1471f1a9ccaccc7fdbf036c4cc41d14
Tiffany Saelinh <saelinht@gmail.com>
2020-05-04 18:47:46 -0700

    Updates

---
+++ precache-manifest.5503727073f979e9efa21c62c33a917c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf34ef47a9e9a1f5cb34bcb1c7f6256",
+    "url": "/host-website/index.html"
+  },
+  {
+    "revision": "5ed77b27ceca6d830cbb",
+    "url": "/host-website/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "465f71aca3c3590717e6",
+    "url": "/host-website/static/css/main.28d26fc4.chunk.css"
+  },
+  {
+    "revision": "5ed77b27ceca6d830cbb",
+    "url": "/host-website/static/js/2.34515507.chunk.js"
+  },
+  {
+    "revision": "3fbebcafe2e76422f28ebbf3e2bf3e4a",
+    "url": "/host-website/static/js/2.34515507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465f71aca3c3590717e6",
+    "url": "/host-website/static/js/main.4dddd388.chunk.js"
+  },
+  {
+    "revision": "ed6d7d981c029cbd95dd",
+    "url": "/host-website/static/js/runtime-main.3fe7bfe5.js"
+  }
+]);
