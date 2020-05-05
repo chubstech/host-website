5faaf63f05c3f1447269c6413181a265661e5974
mycinus <45410115+mycinus@users.noreply.github.com>
2020-05-05 12:48:14 -0700

    Updated Report Chart to multi-line graph

--- src/components/ReportChart.js
+++ src/components/ReportChart.js
@@ -1,200 +1,151 @@
 import React from 'react';
-import { TimeSeries } from "pondjs";
-import _ from "underscore";
-import {
-    Charts,
-    ChartContainer,
-    ChartRow,
-    YAxis,
-    Baseline,
-    Resizable
-  } from "react-timeseries-charts";
-  
- import styler from './../js/styler' 
-import Legend from './../Legend'
-import LineChart from './LineChart'
+import { Line } from 'react-chartjs-2';
+
+const dr_data = require('./../test-data-files/Dr1.json');
+const esp32_data = require('./../test-data-files/Esp32.json');
+const nurse_data = require('./../test-data-files/Nurse1.json');
+const patient_data = require('./../test-data-files/Patient1.json');
+const debug_data = require('./../test-data-files/Debug.json');
 
 
+function getCorrectTimestamp( time_list ) { // Converts any time stamps 13-24 to Clockwise numbers i.e. 1-12
+    const time_stamps = [ [13, 1], [14, 2], [15, 3], [16, 4], [17, 5], [18, 6], [19, 7], [20, 8], [21, 9], [22, 10], [23, 11], [24, 12]];
+    for(let i = 0; i < time_list.length ; i++) {   
+        if( time_list[i][1] > 12 ) {
+            let prevTime = time_list[i][1];
+            for(let a = 0; a < time_stamps.length ; a++) { 
+                if( time_stamps[a][0] == prevTime ) {  time_list[i][1] = time_stamps[a][1] }; 
+        }
+        }
+    }
+    return time_list;
+}
 
-const test_data = require('./../test_data.json');
-
-
-function buildPoints() {
+function getAvgHourlyData( json_data ) { //Gathers the db Levels of each hour from the json and finds avg db level per hr
 
     let result = [];
+    let sec_result = [];
+    let chart_index = [8,9,10,11,12,1,2,3,4,5,6,7,8];
+    let final_result = [0,0,0,0,0,0,0,0,0,0,0,0, 0]
+    let activeHour = 0;
+    let dbAvg = 0;
+    let hourlyNumCount = 0;
 
-    for(let i = 0; i < test_data.length ; i++) {
-
-        var time = new Date( test_data[i].time_obs *100 );
-        result.push( [time,  test_data[i].db_reading, (test_data[i].db_reading -10) ] );
-
+    for(let i = 0; i < json_data.length ; i++) {
+        var time = new Date( json_data[i].time_obs *1000 );
+        if( activeHour == 0 ) {
+            activeHour = time.getHours();
+        } else if ( time.getHours() != activeHour ) {
+            result.push( [Math.round(dbAvg / hourlyNumCount), activeHour ])
+            activeHour = time.getHours();
+            dbAvg = 0;
+            hourlyNumCount = 0;
+        } else if ( i == json_data.length-1 ) {
+            result.push( [Math.round(dbAvg / hourlyNumCount), activeHour ])
+        }
+        dbAvg+= json_data[i].db_reading;
+        hourlyNumCount++;
+        
     }
+    sec_result = getCorrectTimestamp(result);
 
-    return result;
-
+    for( var s = 0; s<sec_result.length; s++){
+            let desiredIndex = chart_index.indexOf( sec_result[s][1] );
+            final_result[ desiredIndex ] = sec_result[s][0];
+    }
+    return final_result;
 
 }
 
-const noiseSeries = new TimeSeries({
-    name: "Noise",
-    columns: ["time", "debug1"],  //columns: ["time", "debug1", "debug2"],
-    points: buildPoints()
-});
+var chartOptions = {
+    showScale: true,
+    pointDot: true,
 
-const style = styler([
-    { key: "debug1", color: "steelblue", width: 3 },
-    // { key: "debug2", color: "#F68B24", width: 2 }
-]);
+    title: {
+        display: true,
+        text: 'Average Noise Levels (Hourly)',
+        fontSize:20
+    },
+    scales: {
+        yAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: 'Noise (dB) Levels'
+            }
+          }]
+       }
+
+}
 
 
 class ReportChart extends React.Component {
 
-
     constructor(props) {
         super(props);
+        this.chartReference = React.createRef();
+        this.label = ['8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM'];
         this.state = {
-            tracker: null,
-            timerange: noiseSeries.range(),
-            x: null,
-            y: null
+            chartData:
+             {
+                labels: this.label,
+                datasets: [ 
+                {
+                    label: "Doctor 1",
+                    fill:false,
+                    backgroundColor: 'rgb(25, 129, 102)',
+                    borderColor: 'rgb(25, 129, 102)',
+                    data: getAvgHourlyData( dr_data ), 
+                },
+                {
+                    label: "Debug",
+                    fill:false,
+                    backgroundColor: 'rgba(171,185,255)',
+                    borderColor: 'rgba(171,185,255)',
+                    data: getAvgHourlyData( debug_data ), 
+                },
+                {
+                    label: "Esp32",
+                    fill:false,
+                    backgroundColor: 'rgb(19, 72, 250)',
+                    borderColor: 'rgb(19, 72, 250)',
+                    data: getAvgHourlyData( esp32_data ), 
+                },
+                {
+                    label: "Nurse1",
+                    fill:false,
+                    backgroundColor: 'rgb(174, 255, 171)',
+                    borderColor: 'rgb(174, 255, 171)',
+                    data: getAvgHourlyData( nurse_data ), 
+                },
+                {
+                    label: "Patient1",
+                    fill:false,
+                    backgroundColor: 'rgb(255, 205, 139)',
+                    borderColor: 'rgb(255, 205, 139)',
+                    data: getAvgHourlyData( patient_data ), 
+                }
+                ]
+             }
         };
 
     }
-    handleTrackerChanged = tracker => {
-        if (!tracker) {
-            this.setState({ tracker, x: null, y: null });
-        } else {
-            this.setState({ tracker });
+
+        render() {
+
+            return (
+
+                <div>
+                    <Line 
+                    ref={this.chartReference}
+                    data={this.state.chartData}
+                    options={chartOptions}
+                    height={240}
+                    width={600}
+                    />
+                </div>
+            );
         }
-    };
-
-    handleTimeRangeChange = timerange => {
-        this.setState({ timerange });
-    };
-
-    handleMouseMove = (x, y) => {
-        this.setState({ x, y });
-    };
-
-    render() {
-        const range = this.state.timerange;
-
-
-        return (
-            <div>
-
-                <p><strong>*Scroll up to Zoom in chart</strong></p>
-
-                <div className="row">
-                    <div className="col-md-12">
-                        <Resizable>
-                            <ChartContainer
-                                timeRange={range}
-                                timeAxisStyle={{
-                                    ticks: {
-                                        stroke: "black",
-                                        opacity: 0.25,
-                                        "stroke-dasharray": "1,1"
-                                        // Note: this isn't in camel case because this is
-                                        // passed into d3's style
-                                    },
-                                    values: {
-                                        fill: "black",
-                                        "font-size": 12
-                                    }
-                                }}
-                                showGrid={true}
-                                paddingRight={125}
-                                maxTime={noiseSeries.range().end()}
-                                minTime={noiseSeries.range().begin()}
-                                timeAxisAngledLabels={true}
-                                timeAxisHeight={65}
-                                onTrackerChanged={this.handleTrackerChanged}
-                                onBackgroundClick={() => this.setState({ selection: null })}
-                                enablePanZoom={true}
-                                onTimeRangeChanged={this.handleTimeRangeChange}
-                                onMouseMove={(x, y) => this.handleMouseMove(x, y)}
-                                minDuration={1000 * 60 * 60 * 24 * 30}
-                            >
-                                <ChartRow height="400">
-                                    <YAxis
-                                        id="y"
-                                        label="(dB) Noise Level"
-                                        min={0}
-                                        max={100}
-                                        style={{
-                                            ticks: {
-                                                stroke: "#AAA",
-                                                opacity: 0.25,
-                                                "stroke-dasharray": "1,1"
-                                                // Note: this isn't in camel case because this is
-                                                // passed into d3's style
-                                            }
-       
-                                        }}
-                                        showGrid
-                                        hideAxisLine
-                                        width="50"
-                                        type="linear"
-                                        format=""
-                                    />
-                                    <Charts>
-                                        <LineChart
-                                            axis="y"
-                                            breakLine={false}
-                                            series={noiseSeries}
-                                            columns={["debug1"]} //columns={["debug1", "debug2"]}
-                                            style={style}
-                                            interpolation="curveBasis"
-                                            highlight={this.state.highlight}
-                                            onHighlightChange={highlight =>
-                                                this.setState({ highlight })
-                                            }
-                                            selection={this.state.selection}
-                                            onSelectionChange={selection =>
-                                                this.setState({ selection })
-                                            }
-                                        />
-                                        <Baseline
-                                            axis="y"
-                                            value={70}
-                                            label="Noise (dB) Level Cap"
-                                            position="right"
-
-                                        />
-                                    </Charts>
-                                </ChartRow>
-                            </ChartContainer>
-                        </Resizable>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <span>
-                            <Legend
-                                type="line"
-                                align="right"
-                                style={style}
-                                highlight={this.state.highlight}
-                                onHighlightChange={highlight => this.setState({ highlight })}
-                                selection={this.state.selection}
-                                onSelectionChange={selection => this.setState({ selection })}
-                                categories={[
-                                    { key: "debug1", label: "User1 (Debug)"},
-                                    // { key: "debug2", label: "User2"}
-                                ]}
-                            />
-                        </span>
-                    </div>
-                </div>
-            </div>
-        );
     }
-}
 
 
-
-
-
-  
   export default ReportChart;
