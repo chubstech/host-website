e59f6376e0724ab084798aac36e4eb0276acb49b
mycinus <45410115+mycinus@users.noreply.github.com>
2020-05-13 15:31:18 -0700

    Updates

---
+++ precache-manifest.e2a203f90a2e3687552d4c07d63af9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7092df344e9b03cce74ae180e780d4",
+    "url": "/host-website/index.html"
+  },
+  {
+    "revision": "c45449114b73c1b70b3a",
+    "url": "/host-website/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c387da1ebd4200e0de45",
+    "url": "/host-website/static/css/main.2aa99924.chunk.css"
+  },
+  {
+    "revision": "c45449114b73c1b70b3a",
+    "url": "/host-website/static/js/2.6162e906.chunk.js"
+  },
+  {
+    "revision": "77c52c67682ebaf3c87f021fd687577f",
+    "url": "/host-website/static/js/2.6162e906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c387da1ebd4200e0de45",
+    "url": "/host-website/static/js/main.edde8383.chunk.js"
+  },
+  {
+    "revision": "ed6d7d981c029cbd95dd",
+    "url": "/host-website/static/js/runtime-main.3fe7bfe5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/host-website/precache-manifest.0b8751df4b27e513bf4cde99937c5682.js"
+  "/host-website/precache-manifest.e2a203f90a2e3687552d4c07d63af9a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/host-website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
