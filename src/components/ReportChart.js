0bb3af13c1b39e37975a63e3fb1959a26252ae46
mycinus <45410115+mycinus@users.noreply.github.com>
2020-05-13 15:28:47 -0700

    Updated Daily Report Chart (API, 24-hr)

--- src/components/ReportChart.js
+++ src/components/ReportChart.js
@@ -1,38 +1,22 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import Button from 'react-bootstrap/Button';
+let color_count = 0;
 
-const dr_data = require('./../test-data-files/Dr1.json');
-const esp32_data = require('./../test-data-files/Esp32.json');
-const nurse_data = require('./../test-data-files/Nurse1.json');
-const patient_data = require('./../test-data-files/Patient1.json');
-const debug_data = require('./../test-data-files/Debug.json');
-
-
-function getCorrectTimestamp( time_list ) { // Converts any time stamps 13-24 to Clockwise numbers i.e. 1-12
-    const time_stamps = [ [13, 1], [14, 2], [15, 3], [16, 4], [17, 5], [18, 6], [19, 7], [20, 8], [21, 9], [22, 10], [23, 11], [24, 12]];
-    for(let i = 0; i < time_list.length ; i++) {   
-        if( time_list[i][1] > 12 ) {
-            let prevTime = time_list[i][1];
-            for(let a = 0; a < time_stamps.length ; a++) { 
-                if( time_stamps[a][0] == prevTime ) {  time_list[i][1] = time_stamps[a][1] }; 
-        }
-        }
-    }
-    return time_list;
-}
 
 function getAvgHourlyData( json_data ) { //Gathers the db Levels of each hour from the json and finds avg db level per hr
 
     let result = [];
-    let sec_result = [];
-    let chart_index = [8,9,10,11,12,1,2,3,4,5,6,7,8];
-    let final_result = [0,0,0,0,0,0,0,0,0,0,0,0, 0]
     let activeHour = 0;
     let dbAvg = 0;
     let hourlyNumCount = 0;
+    // let chart_index = [0,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
+    let final_result = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];//25
+    let activeDate = 0;
 
     for(let i = 0; i < json_data.length ; i++) {
         var time = new Date( json_data[i].time_obs *1000 );
+
         if( activeHour == 0 ) {
             activeHour = time.getHours();
         } else if ( time.getHours() != activeHour ) {
@@ -47,20 +31,33 @@ function getAvgHourlyData( json_data ) { //Gathers the db Levels of each hour fr
         hourlyNumCount++;
         
     }
-    sec_result = getCorrectTimestamp(result);
-
-    for( var s = 0; s<sec_result.length; s++){
-            let desiredIndex = chart_index.indexOf( sec_result[s][1] );
-            final_result[ desiredIndex ] = sec_result[s][0];
+    for( var s = 0; s < result.length; s++){ // Loops through the list to put it on the final list that is shown in the data
+            let desiredIndex = result[s][1] ;
+            final_result[ desiredIndex ] = result[s][0];
     }
     return final_result;
 
 }
 
+function createDataset( user_file, user ) { //Creates a dataset given the json file
+
+    const colors = [ 'rgb(25, 129, 102)', 'rgba(171,185,255)', 'rgb(19, 72, 250)', 'rgb(174, 255, 171)', 'rgb(255, 205, 139)', 'rgb(252, 58, 113)', 'rgb(232, 222, 46)', 'rgb(178, 62, 207)', 'rgb(158, 56, 5)'];
+    let result = {
+        label: user,
+        fill:false,
+        backgroundColor: colors[color_count],
+        borderColor: colors[color_count],
+        data: getAvgHourlyData( user_file ), 
+    };
+    color_count++;
+    return result;
+}
+
+
+
 var chartOptions = {
     showScale: true,
     pointDot: true,
-
     title: {
         display: true,
         text: 'Average Noise Levels (Hourly)',
@@ -77,71 +74,105 @@ var chartOptions = {
 
 }
 
+function makeAPIRequest(userType) {
+    let api_site = 'https://cors-anywhere.herokuapp.com/https://noise-wearable.herokuapp.com/api/noise_observation/user/'+ userType;
+    return fetch( api_site )
+        .then((response) => {
+            return response.json();
+        })
+        .then(json => {
+            var jsonTest = json;
+            return jsonTest;
+        });
+}
+
+
+function makeAPIRequestUsers() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://noise-wearable.herokuapp.com/api/users')
+        .then((response) => {
+            return response.json();
+        })
+        .then(json => {
+            var jsonTest = json;
+            return jsonTest;
+        });
+}
+
+
 
 class ReportChart extends React.Component {
 
     constructor(props) {
         super(props);
         this.chartReference = React.createRef();
-        this.label = ['8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM'];
+        this.label = ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM', '9PM', '10PM', '11PM'];
         this.state = {
+            text: 'Weekly Report',
             chartData:
              {
                 labels: this.label,
-                datasets: [ 
-                {
-                    label: "Doctor 1",
-                    fill:false,
-                    backgroundColor: 'rgb(25, 129, 102)',
-                    borderColor: 'rgb(25, 129, 102)',
-                    data: getAvgHourlyData( dr_data ), 
-                },
-                {
-                    label: "Debug",
-                    fill:false,
-                    backgroundColor: 'rgba(171,185,255)',
-                    borderColor: 'rgba(171,185,255)',
-                    data: getAvgHourlyData( debug_data ), 
-                },
-                {
-                    label: "Esp32",
-                    fill:false,
-                    backgroundColor: 'rgb(19, 72, 250)',
-                    borderColor: 'rgb(19, 72, 250)',
-                    data: getAvgHourlyData( esp32_data ), 
-                },
-                {
-                    label: "Nurse1",
-                    fill:false,
-                    backgroundColor: 'rgb(174, 255, 171)',
-                    borderColor: 'rgb(174, 255, 171)',
-                    data: getAvgHourlyData( nurse_data ), 
-                },
-                {
-                    label: "Patient1",
-                    fill:false,
-                    backgroundColor: 'rgb(255, 205, 139)',
-                    borderColor: 'rgb(255, 205, 139)',
-                    data: getAvgHourlyData( patient_data ), 
-                }
-                ]
-             }
+                datasets: []
+             }, 
+             json_files: []
         };
 
     }
 
+    changeText = () => { //Changes text whenever the button is clicked -- going to add the functionality to switch graphs
+        if ( this.state.text == 'Weekly Report' ) {
+            this.setState({ text: 'Daily Report' }); 
+        } else {
+            this.setState({ text: 'Weekly Report' }); 
+
+        }
+      } 
+
+
+    
+    componentDidMount() { //Sets the state of datasets from the json files that are present
+
+        let dataset_list = [];
+
+        var promiseA = makeAPIRequestUsers().then(devices => { // Makes API request to get current users list
+            var jsonUsers = JSON.stringify(devices);
+            jsonUsers = JSON.parse(jsonUsers); 
+            jsonUsers.forEach((item, i) => {
+                var api_data = makeAPIRequest(item.user_id).then(info => { //Loops through each user to create dataset and pushes it to the state
+                    var json = JSON.stringify(info);
+                    json = JSON.parse(json);
+                    var dataset = createDataset( json, json[0].user_id ); 
+                    dataset_list.push( dataset);   
+                    if ( dataset_list.length == jsonUsers.length) {
+                        this.setState({
+                            chartData: {
+                                datasets: dataset_list
+                            }
+                        });
+                        return dataset_list;
+                    }
+
+                });
+        });
+    
+    });
+    
+}
+
         render() {
-
             return (
-
-                <div>
+                <div className='lineChart'>
                     <Line 
                     ref={this.chartReference}
                     data={this.state.chartData}
                     options={chartOptions}
-                    height={240}
+                    height={250}
                     width={600}
                     />
+
+                    <div class="btn btn-info">
+                    <Button id='reportbutton' variant="info" size="sm" 
+                    onClick={ () => { this.changeText()}  }>{this.state.text}</Button>
+                    </div>
                 </div>
             );
         }
